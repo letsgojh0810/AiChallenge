8d96c8d58057125513b49fbb67d1a4df0992c2d8
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-07-30 20:24:48 +0900

    feat : setting projects

---
+++ src/test/java/com/myapp/aicareercoach/AiCareerCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.aicareercoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCareerCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
